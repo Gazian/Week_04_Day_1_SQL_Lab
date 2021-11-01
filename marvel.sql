21dcaa353d004cd7c69dbeebc166232959de8f1f
Jean-Pierre Gazian <jeanpierregazian@msn.com>
2021-11-01 14:35:41 +0000

    adding completed work and extensions

--- marvel.sql
+++ marvel.sql
@@ -49,3 +49,37 @@ INSERT INTO movies (title, year, show_time) VALUES ('Guardians of the Galaxy 2',
 INSERT INTO movies (title, year, show_time) VALUES ('Spider-Man: Homecoming', 2017, '23:00');
 INSERT INTO movies (title, year, show_time) VALUES ('Thor: Ragnarok', 2017, '22:10');
 INSERT INTO movies (title, year, show_time) VALUES ('Black Panther', 2018, '21:00');
+
+
+--1.  Return ALL the data in the 'movies' table.
+SELECT * FROM movies;
+-- --2.  Return ONLY the name column from the 'people' table
+SELECT name FROM people;
+--3.  Oops! Someone spelled Krusty The Clown's name wrong! Change it to reflect the proper spelling (Crusty should be Krusty).
+UPDATE people SET name = 'Krusty the Clown' where name = 'Crusty the Clown';
+SELECT name FROM people;
+--4.  Return ONLY Homer Simpson's name from the 'people' table.
+SELECT name FROM people where name = 'Homer Simpson';
+--5.  The cinema is showing 'Batman Begins', but Batman is DC, not Marvel! Delete the entry from the 'movies' table.
+DELETE from movies where title = 'Batman Begins';
+SELECT * FROM movies;
+--6.  We forgot one of the main characters! Add Bart Simpson to the 'people' table
+INSERT INTO people (name)
+VALUES ('Bart Simpson');
+SELECT * from people;
+--7.  Eric Cartman has decided to hijack our movie evening, Remove him from the table of people.
+DELETE from people where name = 'Eric Cartman';
+SELECT * FROM people;
+--8.  The cinema has just heard that they will be holding an exclusive midnight showing of 'Avengers: Infinity War'!! Create a new entry in the 'movies' table to reflect this.
+INSERT INTO movies (title,show_time) VALUES ('Avengers: Infinity War','00:00');
+SELECT * FROM movies;
+--9.  The cinema would like to make the Iron Man movies a triple billing. Find out the show time of "Iron Man 2" and set the show time of "Iron Man 3" to start two hours later.
+SELECT * FROM movies
+WHERE title ='Iron Man 2';
+UPDATE movies SET show_time = '20:45'
+WHERE title ='Iron Man 3';
+SELECT * FROM movies;
+
+--Research how to delete multiple entries from your table in a single command.
+DELETE FROM movies WHERE id in (4,5);
+SELECT * FROM movies;
